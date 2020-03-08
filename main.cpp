3aa37ee37a993b92b2661dc93c0a30ef76d82415
vit <miptsu@gmail.com>
2020-03-08 13:13:52 +0300

    + copy data 'host to device' and back + image size is 512x512 now

--- main.cpp
+++ main.cpp
@@ -10,21 +10,24 @@ using namespace cv;
 int main(){
 	const uint fps = 10; // approx frames per second, <=1000, better when 1000%fps=0
 	auto t0 = system_clock::now();
+	UMat img(512, 512, CV_8UC1, Scalar(1)); // UMat uses OpenCL
 	for(ulong i=0; ; i++){
-		const UMat img(50, 200, CV_8UC1, Scalar(i%2));
+		Mat mm(img.rows, img.cols, img.type(), Scalar(0));
 		// dd:hh:mm:ss converting
 		auto s = duration_cast<seconds>(system_clock::now() - t0).count(); // long
-		const auto d = s/(3600*24);
-		s -= d*3600*24;
-		const auto h = s/3600;
-		s -= h*3600;
+		const auto d = s/(60*60*24);
+		s -= d*60*60*24;
+		const auto h = s/(60*60);
+		s -= h*(60*60);
 		const auto m = s/60;
 		s -= m*60;
 		//
 		stringstream ss;
 		ss<<d<<"d "<<h<<"h "<<m<<"m "<<s<<"s"<<(sum(img).val[0] > 1e6 ? "" : " "); // '?:' fools optimization pretending we need sum
-		auto mm = img.getMat(ACCESS_RW);
+		img.copyTo(mm); // device to host copy
+		mm.colRange(0, img.cols-2).setTo(Scalar(0)); // colRange - obfuscating we dont need img data
 		putText(mm, ss.str(), Point(10, 30), FONT_HERSHEY_SIMPLEX, .5, Scalar(150));
+		mm.copyTo(img); // host to device copy (also sum will variate)
 		imshow("uptime", mm);
 		if(waitKey(1000/fps)==27){ break;}
 	}
